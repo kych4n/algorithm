e0d5b3665d24c00d0ea1b4bc68d17873c18736ac
hades3 <164370868+hades3@users.noreply.github.com>
2024-08-03 18:34:22 +0900

    [Gold III] Title: 파티, Time: 112 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -1,56 +1,78 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <cmath>
 #include <algorithm>
 using namespace std;
-int INF = 1e9 + 7;
 
-void Dijkstra(vector<vector<pair<int, int>>> info, vector<int>& dist, int start) {
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-	pq.push({0, start});
-	dist[start] = 0;
+vector<vector<pair<int, int>>> adj_list(1001);
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+vector<int> to_x(1001, 1e9 + 7);
+vector<int> from_x(1001, 1e9 + 7);
+int n, m, x, start, endd, cost, max_cost = 0;
+
+void dijkstra_to_x(int start_x) {
+	vector<int> temp_dist(1001, 1e9 + 7);
+	temp_dist[start_x] = 0;
+	pq.push({ 0, start_x });
 	while (!pq.empty()) {
+		int cur_x = pq.top().second;
 		int cur_cost = pq.top().first;
-		int cur_to = pq.top().second;
 		pq.pop();
-		for (int i = 0; i < info[cur_to].size(); i++) {
-			int new_cost = info[cur_to][i].first;
-			int new_to = info[cur_to][i].second;
-			if (dist[new_to] > cur_cost + new_cost) {
-				dist[new_to] = cur_cost + new_cost;
-				pq.push({ dist[new_to], new_to });
+		for (int i = 0; i < adj_list[cur_x].size(); i++) {
+			int next_x = adj_list[cur_x][i].second;
+			int next_cost = cur_cost + adj_list[cur_x][i].first;
+			if (next_x == x) {
+				to_x[start_x] = min(to_x[start_x], next_cost);
+			}
+			else {
+				if (temp_dist[next_x] > next_cost) {
+					temp_dist[next_x] = next_cost;
+					pq.push({ next_cost, next_x });
+				}
+				
 			}
 		}
 	}
 }
 
-int main(void) {
-	ios::sync_with_stdio(false);
+void dijkstra_from_x(int start_x) {
+	from_x[start_x] = 0;
+	pq.push({ 0, start_x });
+	while (!pq.empty()) {
+		int cur_x = pq.top().second;
+		int cur_cost = pq.top().first;
+		pq.pop();
+		for (int i = 0; i < adj_list[cur_x].size(); i++) {
+			int next_x = adj_list[cur_x][i].second;
+			int next_cost = cur_cost + adj_list[cur_x][i].first;
+			if (from_x[next_x] > next_cost) {
+				from_x[next_x] = next_cost;
+				pq.push({ next_cost, next_x });
+			}
+		}
+	}
+}
+
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
-
-	int n, m, x, from, to, cost;
 	cin >> n >> m >> x;
-	vector<vector<pair<int, int>>> info(n + 1);
 	for (int i = 0; i < m; i++) {
-		cin >> from >> to >> cost;
-		info[from].push_back(make_pair(cost, to));
+		cin >> start >> endd >> cost;
+		adj_list[start].push_back({ cost, endd });
 	}
 
-	vector<int> to_x(n + 1, INF);
 	for (int i = 1; i <= n; i++) {
-		vector<int> dist(n + 1, INF);
-		Dijkstra(info, dist, i);
-		to_x[i] = dist[x];
+		dijkstra_to_x(i);
 	}
-	vector<int> from_x(n + 1, INF);
-	Dijkstra(info, from_x, x);
+	dijkstra_from_x(x);
 
-	int result = 0;
 	for (int i = 1; i <= n; i++) {
-		result = max(result, to_x[i] + from_x[i]);
+		max_cost = max(max_cost, to_x[i] + from_x[i]);
 	}
-	cout << result << '\n';
+	cout << max_cost << "\n";
 
 	return 0;
 }
