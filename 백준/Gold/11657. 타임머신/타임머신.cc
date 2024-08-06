6d48a79e9f9afa800a97a3132f9dd0615fd2c428
hades3 <164370868+hades3@users.noreply.github.com>
2024-08-06 20:50:34 +0900

    [Gold IV] Title: 타임머신, Time: 12 ms, Memory: 2444 KB -BaekjoonHub

---
+++ 백준/Gold/11657. 타임머신/타임머신.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+long long n, m, a, b, c;
+vector<long long> dist(501, 1e9 + 7);
+vector<vector<long long>> edges(6000, vector<long long>(3));
+
+bool bf(int start) {
+	dist[start] = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			long long cur_node = edges[j][0];
+			long long next_node = edges[j][1];
+			long long cost = edges[j][2];
+			if (dist[cur_node] != 1e9+7 && dist[next_node] > dist[cur_node] + cost) {
+				dist[next_node] = dist[cur_node] + cost;
+				if (i == n - 1) {
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < 3; j++) {
+			cin >> edges[i][j];
+		}
+	}
+	
+	bool result = bf(1);
+	if (result) {
+		cout << -1 << "\n";
+	}
+	else {
+		for (int i = 2; i <= n; i++) {
+			if (dist[i] == 1e9 + 7) {
+				cout << -1 << "\n";
+			}
+			else {
+				cout << dist[i] << "\n";
+			}
+		}
+	}
+
+	return 0;
+}
