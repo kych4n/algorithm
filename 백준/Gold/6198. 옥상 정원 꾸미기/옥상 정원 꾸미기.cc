774a1f44922c888a8e6370d57733cafa536b9adc
hades3 <164370868+hades3@users.noreply.github.com>
2024-09-13 16:21:21 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 8 ms, Memory: 2680 KB -BaekjoonHub

---
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+long long n, h, result = 0;
+stack<long long> s;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> h;
+		while (!s.empty() && s.top() <= h) {
+			s.pop();
+		}
+		if (s.empty()) {
+			s.push(h);
+		}
+		else if (s.top() > h) {
+			result += s.size();
+			s.push(h);
+		}
+	}
+	
+
+	cout << result << "\n";
+	return 0;
+}
