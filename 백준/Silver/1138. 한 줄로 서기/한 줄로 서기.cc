27869c3f6ea527bbc3c299b6b5a560e95290ba86
hades3 <164370868+hades3@users.noreply.github.com>
2024-09-25 23:15:22 +0900

    [Silver II] Title: 한 줄로 서기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int n, smaller;
+vector<int> v(10);
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> smaller;
+		for (int j = 0; j < n; j++) {
+			if (v[j] > i || v[j] == 0) {
+				smaller -= 1;
+			}
+			if (smaller == -1) {
+				v[j] = i;
+				break;
+			}
+		}
+	}
+	
+	for (int i = 0; i < n; i++) {
+		cout << v[i] << " ";
+	}
+
+
+	return 0;
+}
