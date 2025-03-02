2e5bc1de28d35fd65920cd598a1c1d147530db1a
kych4n <164370868+kych4n@users.noreply.github.com>
2025-03-02 17:46:04 +0900

    [Silver V] Title: 단어 정렬, Time: 80 ms, Memory: 6624 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,37 +1,34 @@
 #include <iostream>
+#include <set>
 #include <vector>
-#include <queue>
 #include <algorithm>
 using namespace std;
 
 int n;
-string s;
-vector<string> words;
+string word;
+set<string> words_set;
+
 bool compare(string a, string b) {
-	if (a.length() < b.length()) {
-		return true;
-	}
-	else if (a.length() == b.length()) {
-		return a < b;
-	}
-	else {
-		return false;
-	}
+    if (a.length() < b.length()) {
+        return true;
+    } else if (a.length() == b.length()) {
+        return a < b;
+    }
+    else {
+        return false;
+    }
 }
 
-int main(void)
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> s;
-		words.push_back(s);
-	}
-	sort(words.begin(), words.end(), compare);
-	words.erase(unique(words.begin(), words.end()), words.end());
-	for (int i = 0; i < words.size(); i++) {
-		cout << words[i] << "\n";
-	}
-	return 0;
+int main() {
+    cin >> n;
+    for (int i=0; i<n; i++) {
+        cin >> word;
+        words_set.insert(word);
+    }
+    vector<string> words_vector(words_set.begin(), words_set.end());
+    sort(words_vector.begin(), words_vector.end(), compare);
+    for (string word : words_vector) {
+        cout << word << "\n";
+    }
+    return 0;
 }
