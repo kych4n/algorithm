7f50b2f8963f70ac4b4b48f3b146323104e5c486
kych4n <164370868+kych4n@users.noreply.github.com>
2025-03-10 23:43:35 +0900

    [Silver III] Title: 프린터 큐, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.cc
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.cc
@@ -1,42 +1,36 @@
 #include <iostream>
 #include <queue>
-#include <cmath>
-#include <algorithm>
 using namespace std;
 
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	int t, n, m, rank;
-	cin >> t;
-	for (int i = 0; i < t; i++) {
-		priority_queue<int> pq;
-		queue<pair<int, int>> q;
-		cin >> n >> m;
-		for (int j = 0; j < n; j++) {
-			cin >> rank;
-			pq.push(rank);
-			q.push(make_pair(rank, j));
-		}
-		int count = 1;
-		while (true) {
-			if (q.front().first == pq.top()) {
-				if (q.front().second == m) {
-					cout << count << "\n";
-					break;
-				}
-				else {
-					pq.pop();
-					q.pop();
-					count++;
-				}
-			}
-			else {
-				q.push(q.front());
-				q.pop();
-			}
-		}
-	}
-	
-	return 0;
+int t, n, m, document;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        priority_queue<int> documents_by_importance;
+        queue<pair<int, int> > documents_by_inserted_order;
+        cin >> n >> m;
+        for (int j = 0; j < n; j++) {
+            cin >> document;
+            documents_by_importance.push(document);
+            documents_by_inserted_order.emplace(document, j);
+        }
+        while (!documents_by_importance.empty()) {
+            if (documents_by_importance.top() > documents_by_inserted_order.front().first) {
+                documents_by_inserted_order.push(documents_by_inserted_order.front());
+                documents_by_inserted_order.pop();
+            } else {
+                if (documents_by_inserted_order.front().second == m) {
+                    cout << n - documents_by_importance.size() + 1 << "\n";
+                    break;
+                }
+                documents_by_importance.pop();
+                documents_by_inserted_order.pop();
+            }
+        }
+    }
+
+    return 0;
 }
