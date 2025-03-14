f9507142890387a0cca5a8864c7138cc0c024973
kych4n <164370868+kych4n@users.noreply.github.com>
2025-03-14 11:58:06 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int solve(int n) {
+    int count_five = n / 5;
+    int count_three = n % 5 / 3;
+    while (count_five > 0 && count_five * 5 + count_three * 3 != n) {
+        count_five -= 1;
+        count_three += 2;
+    }
+    if (count_five * 5 + count_three * 3 == n) {
+        return count_five + count_three;
+    }
+    return -1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    cout << solve(n) << "\n";
+    return 0;
+}
