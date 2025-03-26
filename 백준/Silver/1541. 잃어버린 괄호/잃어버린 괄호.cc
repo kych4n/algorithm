179f261c3a4d87683ce473730130d31d48eb6586
kych4n <164370868+kych4n@users.noreply.github.com>
2025-03-26 14:29:34 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -1,35 +1,34 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <cmath>
-using namespace std;	
-string str, temp = "";
-bool minus_appeared = false;
-int result = 0;
+using namespace std;
+
+string expression;
+
+int solve(string expression) {
+    int result = 0;
+    string number = "";
+    bool minusSignAppeared = false;
+    for (char character: expression) {
+        if (!(character == '+' || character == '-')) {
+            number += character;
+        } else {
+            if (minusSignAppeared) {
+                result -= stoi(number);
+            } else {
+                result += stoi(number);
+            }
+            number = "";
+            if (character == '-') {
+                minusSignAppeared = true;
+            }
+        }
+    }
+    return result;
+}
 
 int main() {
-	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
-	cin >> str;
-	str += '+';
-	for (int i=0; i<str.length(); i++){
-		if (!(str[i] == '+' || str[i] == '-')){
-			temp += str[i];
-		}
-		else {
-			if (minus_appeared){
-				result -= stoi(temp);
-			}
-			else{
-				result += stoi(temp);
-			}
-			if (str[i] == '-'){
-				minus_appeared = true;
-			}
-			temp = "";
-		}
-	}
-	cout << result << "\n";
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> expression;
+    cout << solve(expression + "+") << "\n";
+    return 0;
 }
