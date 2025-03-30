bdbb0ca3490a80a8f8cc626564903a3e35db481a
kych4n <164370868+kych4n@users.noreply.github.com>
2025-03-30 21:27:46 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 216 ms, Memory: 31732 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,32 +1,32 @@
 #include <iostream>
-#include <vector>
 #include <map>
-#include <algorithm>
-#include <cmath>
-using namespace std;	
-int n, m;
-string name, target;
-map<string, int> dogam1;
-map<int, string> dogam2;
+using namespace std;
+
+map<string, string> book;
+
+void getInput(int n) {
+    string name;
+    for (int number = 1; number <= n; number++) {
+        cin >> name;
+        book[name] = to_string(number);
+        book[to_string(number)] = name;
+    }
+}
+
+void solve(int m) {
+    string query;
+    for (int i = 0; i < m; i++) {
+        cin >> query;
+        cout << book[query] << "\n";
+    }
+}
 
 int main() {
-	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
-	cin >> n >> m;
-	for (int i=1; i<=n; i++){
-		cin >> name;
-		dogam1.insert({name, i});
-		dogam2.insert({i, name});
-	}
-	for (int i=1; i<=m; i++){
-		cin >> target;
-		if (dogam1.find(target) == dogam1.end()) {
-			cout << dogam2.find(stoi(target))->second << "\n";
-		}
-		else{
-			cout << dogam1.find(target)->second << "\n";
-		}
-	}
-	
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m;
+    cin >> n >> m;
+    getInput(n);
+    solve(m);
+    return 0;
 }
