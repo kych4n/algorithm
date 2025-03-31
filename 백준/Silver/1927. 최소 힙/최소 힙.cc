9984155dc26a2e225de692d0f37705c41c6eb84e
kych4n <164370868+kych4n@users.noreply.github.com>
2025-03-31 22:52:26 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.cc
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -1,30 +1,30 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-#include <algorithm>
-#include <cmath>
-using namespace std;	
-int n, x;
-priority_queue<int, vector<int>, greater<int>> pq;
+using namespace std;
+
+void solve(int n) {
+    int x;
+    priority_queue<int, vector<int>, greater<int> > pq;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        if (x == 0) {
+            if (pq.empty()) {
+                cout << 0 << "\n";
+            } else {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        } else {
+            pq.push(x);
+        }
+    }
+}
 
 int main() {
-	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
-	cin >> n;
-	for (int i=0; i<n; i++){
-		cin >> x;
-		if (x==0){
-			if (pq.empty()){
-				cout << 0 << "\n";
-			}
-			else{
-				cout << pq.top() << "\n";
-				pq.pop();
-			}
-		}
-		else{
-			pq.push(x);
-		}
-	}
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    solve(n);
+    return 0;
 }
