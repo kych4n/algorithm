83048e2104bd6a9c7809853e8b2c214d8176d731
kych4n <164370868+kych4n@users.noreply.github.com>
2025-04-02 18:20:55 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,49 +1,43 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-#include <algorithm>
-#include <cmath>
-using namespace std;	
-int n, m;
-int dx[] = {-1,1,0,0};
-int dy[] = {0,0,-1,1};
-string temp;
-vector<vector<int>> miro(100, vector<int>(100));
-vector<vector<int>> dp(100, vector<int>(100, 1e9+7));
-queue<pair<int, int>> q;
+using namespace std;
 
-void bfs(){
-	dp[0][0] = 1;
-	q.push({0, 0});
-	while (!q.empty()){
-		int cur_x = q.front().first;
-		int cur_y = q.front().second;
-		q.pop();
-		for (int i=0; i<4; i++){
-			int new_x = cur_x + dx[i];
-			int new_y = cur_y + dy[i];
-			if (!(new_x >= 0 && new_x < n && new_y >=0 && new_y < m) || !miro[new_x][new_y]) {
-				continue;
-			}
-			if (dp[new_x][new_y] > dp[cur_x][cur_y] + 1){
-				q.push({new_x, new_y});
-				dp[new_x][new_y] = dp[cur_x][cur_y] + 1;
-			}
-		}
-	}
+vector<string> maze;
+int dy[] = {-1, 1, 0, 0};
+int dx[] = {0, 0, -1, 1};
+
+int solve(int n, int m) {
+    vector<vector<int> > minCount(n, vector<int>(m, 1e9 + 7));
+    minCount[0][0] = 1;
+    queue<pair<int, int> > q;
+    q.push({0, 0});
+    while (!q.empty()) {
+        int curY = q.front().first;
+        int curX = q.front().second;
+        q.pop();
+        for (int i = 0; i < 4; i++) {
+            int newY = curY + dy[i];
+            int newX = curX + dx[i];
+            if ((newY >= 0 && newY < n && newX >= 0 && newX < m) && (maze[newY][newX] == '1') && (
+                    minCount[curY][curX] + 1 < minCount[newY][newX])) {
+                minCount[newY][newX] = minCount[curY][curX] + 1;
+                q.push({newY, newX});
+            }
+        }
+    }
+    return minCount[n - 1][m - 1];
 }
 
 int main() {
-	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
-	cin >> n >> m;
-	for (int i=0; i<n; i++){
-		cin >> temp;
-		for (int j=0; j<m; j++){
-			miro[i][j] = temp[j] - '0';
-		}
-	}
-	bfs();
-	cout << dp[n-1][m-1] << "\n";
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m;
+    string row;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> row;
+        maze.push_back(row);
+    }
+    cout << solve(n, m) << "\n";
+    return 0;
 }
