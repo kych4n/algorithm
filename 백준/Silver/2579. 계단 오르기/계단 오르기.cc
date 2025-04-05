e4773c9686dd81e0e4fa3c81320c6fcb2af266a3
kych4n <164370868+kych4n@users.noreply.github.com>
2025-04-05 22:42:42 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,31 +1,33 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <algorithm>
-#include <cmath>
-using namespace std;	
-int n;
-vector<int> stage(301);
-vector<vector<int>> dp(301, vector<int>(3));
+using namespace std;
+
+vector<int> scores(1);
+
+int solve(int n) {
+    vector<pair<int, int> > maxScores(n + 1);
+    maxScores[n] = {scores[n], 0};
+    for (int i = n; i > 0; i--) {
+        if (i - 2 >= 0) {
+            maxScores[i - 2].first = max(maxScores[i - 2].first,
+                                         max(maxScores[i].first, maxScores[i].second) + scores[i - 2]);
+        }
+        if (i - 1 >= 0) {
+            maxScores[i - 1].second = max(maxScores[i - 1].second, maxScores[i].first + scores[i - 1]);
+        }
+    }
+    return max(max(maxScores[0].first, maxScores[0].second), max(maxScores[1].first, maxScores[1].second));
+}
 
 int main() {
-	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
-	
-	cin >> n;
-	for (int i=1; i<=n; i++){
-		cin >> stage[i];
-	}
-
-	dp[1][1] = stage[1];	// 시작점은 계단으로 포함시키지 않기 때문에 첫번째 계단까지 연속 횟수는 첫번째 계단만을 고려한 1
-	dp[1][0] = stage[1];
-	for (int i=2; i<=n; i++){
-		dp[i][1] = max(dp[i-2][1], dp[i-2][2]) + stage[i];
-		dp[i][2] = dp[i-1][1] + stage[i];
-		dp[i][0] = max(dp[i][1], dp[i][2]);
-	}
-
-	cout << dp[n][0] << "\n";
-
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, score;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> score;
+        scores.push_back(score);
+    }
+    cout << solve(n) << "\n";
+    return 0;
 }
