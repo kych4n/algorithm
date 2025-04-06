99f97233549adf143d765bb8f3fab2b51f8d5c74
kych4n <164370868+kych4n@users.noreply.github.com>
2025-04-06 14:30:51 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -1,42 +1,42 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <algorithm>
-#include <cmath>
-using namespace std;	
-int n, m, a, b, result=0;
-vector<vector<int>> adj_list(101);
-vector<bool> visited(101);
-queue<int> q;
+using namespace std;
 
-void bfs(){
-	visited[1] = true;
-	q.push(1);
-	while (!q.empty()){
-		int x = q.front();
-		q.pop();
-		for (int i=0; i<adj_list[x].size(); i++){
-			int y = adj_list[x][i];
-			if (!visited[y]){
-				q.push(y);
-				visited[y] = true;
-				result += 1;
-			}
-		}
-	}
+vector<vector<int> > linked(101);
+
+int solve() {
+    vector<int> infected(101);
+    infected[1] = 1;
+    queue<int> q;
+    q.push(1);
+    while (!q.empty()) {
+        int curCom = q.front();
+        q.pop();
+        for (int nextCom: linked[curCom]) {
+            if (!infected[nextCom]) {
+                infected[nextCom] = 1;
+                q.push(nextCom);
+            }
+        }
+    }
+    int infectedCount = -1;
+    for (int infectedCom: infected) {
+        infectedCount += infectedCom;
+    }
+    return infectedCount;
 }
 
 int main() {
-	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
-	cin >> n >> m;
-	for (int i=0; i<m; i++){
-		cin >> a >> b;
-		adj_list[a].push_back(b);
-		adj_list[b].push_back(a);
-	}
-	bfs();
-	cout << result << "\n";
-
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m, com1, com2;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> com1 >> com2;
+        linked[com1].push_back(com2);
+        linked[com2].push_back(com1);
+    }
+    cout << solve() << "\n";
+    return 0;
 }
