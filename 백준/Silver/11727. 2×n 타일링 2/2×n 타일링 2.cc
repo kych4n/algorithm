d48ee9b837885c3c4f13cd6332faf4d41b1ede84
kych4n <164370868+kych4n@users.noreply.github.com>
2025-04-14 23:46:56 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -1,23 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <queue>
 using namespace std;
 
-int n;
-vector<int> dp(1001);
+constexpr int DIVISION = 10007;
 
-int main(void)
-{	
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cin >> n;
-	dp[1] = 1;
-	dp[2] = 3;
-	for (int i = 3; i <= n; i++) {
-		dp[i] = dp[i - 1] + dp[i - 2] * 2;
-		dp[i] %= 10007;
-	}
-	cout << dp[n] << "\n";
-	return 0;
+int solve(int n) {
+    vector<int> dp(1001);
+    dp[1] = 1;
+    dp[2] = 3;
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i] + 2 * dp[i - 2]) % DIVISION;
+        dp[i] = (dp[i] + dp[i - 1]) % DIVISION;
+    }
+    return dp[n];
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    cout << solve(n) << "\n";
+    return 0;
 }
