83f7679218d8e3c2b6d477891bfe3113b5f7f43e
kych4n <164370868+kych4n@users.noreply.github.com>
2025-04-27 23:05:01 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,28 +1,25 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
 
-int t, n;
-vector<int> dp(11);
+vector<int> dp(12);
 
-int main(void)
-{	
-	dp[0] = 1;
-	for (int i = 1; i < 11; i++) {
-		if (i - 1 >= 0) {
-			dp[i] += dp[i - 1];
-		}
-		if (i - 2 >= 0) {
-			dp[i] += dp[i - 2];
-		}
-		if (i - 3 >= 0) {
-			dp[i] += dp[i - 3];
-		}
-	}
-	cin >> t;
-	for (int i = 0; i < t; i++) {
-		cin >> n;
-		cout << dp[n] << "\n";
-	}
-	return 0;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t, n;
+    cin >> t;
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+    for (int i = 4; i <= 11; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+    }
+    for (int i = 0; i < t; i++) {
+        cin >> n;
+        cout << dp[n] << "\n";
+    }
+
+    return 0;
 }
