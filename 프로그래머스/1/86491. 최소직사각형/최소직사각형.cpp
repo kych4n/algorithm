442bb175a2bb6ef12ba795aaf42d6e3e61f5b791
kych4n <164370868+kych4n@users.noreply.github.com>
2025-06-27 20:02:16 +0900

    [level 1] Title: 최소직사각형, Time: 0.67 ms, Memory: 5 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -8,14 +8,8 @@ int solution(vector<vector<int>> sizes) {
     int greaterValue = 0;
     int lessValue = 0;
     for (int i=0; i<sizes.size(); i++) {
-        if (sizes[i][0] > sizes[i][1]) {
-            greaterValue = max(greaterValue, sizes[i][0]);
-            lessValue = max(lessValue, sizes[i][1]);
-        } else {
-            greaterValue = max(greaterValue, sizes[i][1]);
-            lessValue = max(lessValue, sizes[i][0]);
-        }
-        
+        greaterValue = max(greaterValue, max(sizes[i][0], sizes[i][1]));
+        lessValue = max(lessValue, min(sizes[i][0], sizes[i][1]));
     }
     answer = greaterValue * lessValue;
     return answer;
