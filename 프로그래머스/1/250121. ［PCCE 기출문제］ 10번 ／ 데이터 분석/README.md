0c550c61d90041bd4876bab9258ec794718d1c31
kych4n <164370868+kych4n@users.noreply.github.com>
2025-08-08 19:16:19 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.54 ms, Memory: 4.03 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/README.md
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/README.md
@@ -1,10 +1,10 @@
 # [level 1] [PCCE 기출문제] 10번 / 데이터 분석 - 250121 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250121?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250121) 
 
 ### 성능 요약
 
-메모리: 4.32 MB, 시간: 0.56 ms
+메모리: 4.03 MB, 시간: 0.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 08일 19:07:12
+2025년 08월 08일 19:16:23
 
 ### 문제 설명
 
--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.cpp
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.cpp
@@ -11,11 +11,8 @@ vector<vector<int>> solution(vector<vector<int>> data, string ext, int val_ext, 
     vector<vector<int>> filtered;
     int extIndex = mapping[ext];
     int sortIndex = mapping[sort_by];
-    for (vector<int> datum: data) {
-        if (datum[extIndex] < val_ext) {
-            filtered.push_back(datum);
-        }
-    }
-    sort(filtered.begin(), filtered.end(), [sortIndex](const vector<int>& a, const vector<int>& b) { return a[sortIndex] < b[sortIndex]; });
-    return filtered;
+    auto it = remove_if(data.begin(), data.end(), [extIndex, val_ext](const vector<int>& a){ return a[extIndex] >= val_ext; });
+    data.erase(it, data.end());
+    sort(data.begin(), data.end(), [sortIndex](const vector<int>& a, const vector<int>& b) { return a[sortIndex] < b[sortIndex]; });
+    return data;
 }
