3ed527e891fc28bd2283be5550de271a275c04ed
kych4n <164370868+kych4n@users.noreply.github.com>
2025-08-14 22:09:11 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.cc
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
-#include <algorithm>
 using namespace std;
 
 int main(void) {
@@ -9,9 +7,8 @@ int main(void) {
 	cin.tie(NULL);
 	int n;
 	cin >> n;
-	vector<bool> dp(1001);
+	vector<bool> dp(n + 1);
 	dp[1] = true;
-	dp[2] = false;
 	dp[3] = true;
 	for (int i = 5; i <= n; i++) {
 		dp[i] = !dp[i - 1] || !dp[i - 3];
