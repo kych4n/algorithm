5c7517b05271a7d7c578d451ff1604f5a79fc3a5
kych4n <164370868+kych4n@users.noreply.github.com>
2025-08-17 19:17:09 +0900

    [Silver IV] Title: 등수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.cc
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.cc
@@ -1,54 +1,31 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <map>
-#include <set>
-#include <queue>
 #include <algorithm>
 using namespace std;
 
-int p, n, score, new_score;
+int n, my_score, score, p, ranking = 1;
 vector<int> scores;
 
-int main(void)
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-
-	cin >> n >> new_score >> p;
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> n >> my_score >> p;
 	for (int i = 0; i < n; i++) {
 		cin >> score;
 		scores.push_back(score);
 	}
-
-	if (n == 0) {
-		cout << 1 << "\n";
+	if (n == p && scores.back() >= my_score) {
+		cout << -1 << "\n";
 		return 0;
 	}
-
 	for (int i = 0; i < n; i++) {
-		if (new_score >= scores[i] && new_score != scores.back()) {
-			cout << i + 1 << "\n";
-			return 0;
-		}
-	}
-
-	if (n == p) {
-		cout << -1 << "\n";
-	}
-	else {
-		if (new_score == scores.back()) {
-			for (int i = 0; i < n; i++) {
-				if (new_score == scores[i]) {
-					cout << i + 1 << "\n";
-					return 0;
-				}
-			}
-		}
+		if (scores[i] > my_score) {
+			ranking += 1;
+		} 
 		else {
-			cout << n + 1 << "\n";
+			break;
 		}
 	}
-
+	cout << ranking << "\n";
 	return 0;
 }
