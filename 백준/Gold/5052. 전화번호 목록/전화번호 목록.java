116cd8dbafb99a6f5c789b209634e1093b1f072a
kych4n <164370868+kych4n@users.noreply.github.com>
2025-11-16 00:49:43 +0900

    [Gold IV] Title: 전화번호 목록, Time: 556 ms, Memory: 40576 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -12,23 +12,18 @@ public class Main {
             String answer = "YES";
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
             int N = Integer.parseInt(stringTokenizer.nextToken());
-            Set<String> phoneNumberSet = new HashSet<>();
             String[] phoneNumberList = new String[N];
             for (int i=0; i<N; i++) {
                 stringTokenizer = new StringTokenizer(bufferedReader.readLine());
                 String phoneNumber = stringTokenizer.nextToken();
-                phoneNumberSet.add(phoneNumber);
                 phoneNumberList[i] = phoneNumber;
             }
 
-            for (int i=0; i<N; i++) {
-                String phoneNumber = phoneNumberList[i];
-                for (int j=1; j<phoneNumber.length(); j++) {
-                    String target = phoneNumber.substring(0, j);
-                    if (phoneNumberSet.contains(target)) {
-                        answer = "NO";
-                        break;
-                    }
+            Arrays.sort(phoneNumberList);
+            for (int i=0; i<N-1; i++) {
+                if (phoneNumberList[i+1].startsWith(phoneNumberList[i])) {
+                    answer = "NO";
+                    break;
                 }
             }
 
