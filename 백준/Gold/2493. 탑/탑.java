3c2657bba38c38c5aa5ceadfa9954510383d4450
kych4n <164370868+kych4n@users.noreply.github.com>
2025-11-16 00:19:28 +0900

    [Gold V] Title: 탑, Time: 568 ms, Memory: 114156 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -5,8 +5,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        StringBuilder stringBuilder = new StringBuilder();
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
         int N = Integer.parseInt(stringTokenizer.nextToken());
         int[] tower = new int[N];
@@ -15,7 +14,7 @@ public class Main {
             tower[i] = Integer.parseInt(stringTokenizer.nextToken());
         }
 
-        Stack<Tower> s = new Stack<>();
+        Deque<Tower> s = new ArrayDeque<>();
         for (int i=0; i<N; i++) {
             int height = tower[i];
             while (!s.isEmpty() && s.peek().height < height) {
@@ -23,17 +22,16 @@ public class Main {
             }
             if (!s.isEmpty()) {
                 int receivedTower = s.peek().id;
-                bufferedWriter.write(receivedTower + " ");
+                stringBuilder.append(receivedTower);
             }
             else {
-                bufferedWriter.write(0 + " ");
+                stringBuilder.append(0);
             }
-            s.add(new Tower(i+1, height));
+            stringBuilder.append(" ");
+            s.push(new Tower(i+1, height));
         }
-
-        bufferedWriter.close();
+        System.out.println(stringBuilder);
     }
-
 }
 
 class Tower {
