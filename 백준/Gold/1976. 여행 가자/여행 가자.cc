ad685f6bf7e9de75a525e3cc10292871666604ec
kych4n <164370868+kych4n@users.noreply.github.com>
2026-01-27 19:48:02 +0900

    [Gold IV] Title: 여행 가자, Time: 0 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.cc
+++ 백준/Gold/1976. 여행 가자/여행 가자.cc
@@ -6,6 +6,25 @@ using namespace std;
 int N, M;
 vector<vector<int>> adj_list;
 vector<int> plan;
+vector<int> parent;
+
+int find_parent(int x) {
+	if (x != parent[x]) {
+		return find_parent(parent[x]);
+	}
+	return parent[x];
+}
+
+void union_parent(int x, int y) {
+	x = find_parent(x);
+	y = find_parent(y);
+	if (x < y) {
+		parent[y] = x;
+	}
+	else {
+		parent[x] = y;
+	}
+}
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -13,32 +32,30 @@ int main() {
 
 	cin >> N >> M;
 	adj_list.resize(N, vector<int>(N));
+	parent.resize(N);
 	plan.resize(M);
 	for (int i = 0; i < N; i++) {
+		parent[i] = i;
 		for (int j = 0; j < N; j++) {
 			cin >> adj_list[i][j];
 		}
-	}
-	for (int i = 0; i < N; i++) {
-		adj_list[i][i] = 1;
 	}
 	for (int i = 0; i < M; i++) {
 		cin >> plan[i];
 		plan[i]--;
 	}
-
-	for (int k = 0; k < N; k++) {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				adj_list[i][j] = max(adj_list[i][j], adj_list[i][k] * adj_list[k][j]);
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (adj_list[i][j] && parent[i] != parent[j]) {
+				union_parent(i, j);
 			}
 		}
 	}
-	
+
 	for (int i = 0; i < M - 1; i++) {
 		int start = plan[i];
 		int end = plan[i + 1];
-		if (!adj_list[start][end]) {
+		if (find_parent(start) != find_parent(end)) {
 			cout << "NO" << "\n";
 			return 0;
 		}
