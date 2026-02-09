be412a22ef17f646d6b3ad3229300d1c7eecac4a
kych4n <164370868+kych4n@users.noreply.github.com>
2026-02-09 17:29:51 +0900

    [Gold V] Title: 탑, Time: 88 ms, Memory: 8292 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int N;
+    cin >> N;
+    vector<pair<int, int>> building;
+    for (int i = 1; i <= N; i++) {
+        int h;
+        cin >> h;
+        if (building.empty()) {
+            cout << 0 << " ";
+            building.push_back({h, i});
+            continue;
+        }
+        else {
+            while (!building.empty() && building.back().first < h) {
+                building.pop_back();
+            }
+            if (building.empty()) {
+                cout << 0 << " ";
+            }
+            else {
+                cout << building.back().second << " ";
+            }
+            building.push_back({ h, i });
+        }
+    }
+
+    return 0;
+}
