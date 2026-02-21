38ac92425e0d714a8472f7c65819de4b05c693bd
kych4n <164370868+kych4n@users.noreply.github.com>
2026-02-21 13:54:59 +0900

    [Gold IV] Title: 알파벳, Time: 488 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -3,47 +3,41 @@
 #include <algorithm>
 using namespace std;
 
-int r, c, result = 0;
-int dx[] = { -1,1,0,0 };
-int dy[] = { 0,0,-1,1 };
-vector<vector<char>> board(20, vector<char>(20));
+int R, C, result = 1;
+int dr[] = { -1,1,0,0 };
+int dc[] = { 0,0,-1,1 };
+vector<string> board;
 vector<bool> visited(26);
-string s;
 
-void dfs(int cur_x, int cur_y, int temp_result) {
-	result = max(result, temp_result);
-
-	for (int i = 0; i < 4; i++) {
-		int new_x = cur_x + dx[i];
-		int new_y = cur_y + dy[i];
-		if (!(new_x >= 0 && new_x < r && new_y >= 0 && new_y < c)) {
-			continue;
-		}
-		int new_alphabet = board[new_x][new_y];
-		if (!visited[new_alphabet - 'A']) {
-			visited[new_alphabet - 'A'] = true;
-			dfs(new_x, new_y, temp_result + 1);
-			visited[new_alphabet - 'A'] = false;
-		}
-	}
+void bt(int cur_r, int cur_c, int temp_result) {
+    for (int i = 0; i < 4; i++) {
+        int new_r = cur_r + dr[i];
+        int new_c = cur_c + dc[i];
+        if (!(new_r >= 0 && new_r < R && new_c >= 0 && new_c < C)) continue;
+        char new_char = board[new_r][new_c];
+        if (!visited[new_char - 'A']) {
+            visited[new_char - 'A'] = true;
+            result = max(result, temp_result + 1);
+            bt(new_r, new_c, temp_result + 1);
+            visited[new_char - 'A'] = false;
+        }
+    }
 }
 
-int main(void)
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cin >> r >> c;
-	for (int i = 0; i < r; i++) {
-		cin >> s;
-		for (int j = 0; j < c; j++) {
-			board[i][j] = s[j];
-		}
-	}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	visited[board[0][0] - 'A'] = true;
-	dfs(0, 0, 1);
+    cin >> R >> C;
+    board.resize(R);
+    for (int i = 0; i < R; i++) {
+        cin >> board[i];
+    }
 
-	cout << result << "\n";
+    visited[board[0][0] - 'A'] = true;
+    bt(0, 0, 1);
 
-	return 0;
+    cout << result << "\n";
+
+    return 0;
 }
