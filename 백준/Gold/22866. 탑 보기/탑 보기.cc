b3e416998e0715049ecaef344210d5026490e65e
kych4n <164370868+kych4n@users.noreply.github.com>
2026-03-05 11:55:59 +0900

    [Gold III] Title: 탑 보기, Time: 24 ms, Memory: 4124 KB -BaekjoonHub

--- 백준/Gold/22866. 탑 보기/탑 보기.cc
+++ 백준/Gold/22866. 탑 보기/탑 보기.cc
@@ -4,64 +4,64 @@
 #include <algorithm>
 using namespace std;
 
-struct Info {
-	int min_number = 1e9 + 7;
-	int visible_count = 0;
+struct Result {
+    int min_number = 1e9 + 7;
+    int visible_count;
 };
-
 int N;
-vector<int> height;
-vector<Info> info;
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    /*
+        - 어떤 건물이 볼 수 있는 건물의 개수, 가장 가까운 건물의 번호 중 작은 것
+        - O(10**5) -> O(N)
+        - 왼쪽, 오른쪽 나눠서 생각
+        - 어떤 방향으로 높이가 L인 건물이 있다면, 그 뒤에 있는 건물은 영향이 없음
+        - 자신과 가까운 것에 큰 영향을 받음 -> 스택
 
-	cin >> N;
-	height.resize(N);
-	info.resize(N);
-	for (int i = 0; i < N; i++) {
-		cin >> height[i];
-	}
+        - 현재 높이보다 작은 top들을 모두 pop -> 남은 개수 = 볼 수 있는 건물의 개수
+    */
+    cin >> N;
+    vector<int> height(N + 1);
+    for (int i = 1; i <= N; i++) {
+        cin >> height[i];
+    }
 
-	// left -> right
-	stack<pair<int, int>> s1;
-	s1.push({ height[N - 1], N - 1 });
-	for (int i = N - 2; i >= 0; i--) {
-		while (!s1.empty() && height[i] >= s1.top().first) {
-			s1.pop();
-		}
-		info[i].visible_count += s1.size();
-		if (!s1.empty()) {
-			info[i].min_number = min(info[i].min_number, s1.top().second);
-		}
-		s1.push({ height[i], i });
-	}
+    vector<Result> result(N + 1);
 
-	// right -> left
-	stack<pair<int, int>> s2;
-	s2.push({ height[0], 0 });
-	for (int i = 1; i < N; i++) {
-		while (!s2.empty() && height[i] >= s2.top().first) {
-			s2.pop();
-		}
-		info[i].visible_count += s2.size();
-		if (!s2.empty()) {
-			if (abs(info[i].min_number - i) >= abs(s2.top().second - i)) {
-				info[i].min_number = min(info[i].min_number, s2.top().second);
-			}
-		}
-		s2.push({ height[i], i });
-	}
+    // 왼쪽
+    stack<pair<int, int>> left;
+    for (int i = 1; i <= N; i++) {
+        while (!left.empty() && left.top().first <= height[i]) left.pop();
+        result[i].visible_count += left.size();
+        if (!left.empty()) result[i].min_number = left.top().second;
+        left.push({ height[i], i });
+    }
 
-	for (int i = 0; i < N; i++) {
-		if (info[i].visible_count == 0) {
-			cout << info[i].visible_count << "\n";
-		}
-		else {
-			cout << info[i].visible_count << " " << info[i].min_number + 1 << "\n";
-		}
-	}
+    // 오른쪽
+    stack<pair<int, int>> right;
+    for (int i = N; i >= 1; i--) {
+        while (!right.empty() && right.top().first <= height[i]) right.pop();
+        result[i].visible_count += right.size();
+        if (!right.empty()) {
+            if (abs(result[i].min_number - i) > abs(i - right.top().second)) {
+                result[i].min_number = right.top().second;
+            }
+        }
+        right.push({ height[i], i });
+    }
 
-	return 0;
+    for (int i = 1; i <= N; i++) {
+        cout << result[i].visible_count;
+        if (result[i].visible_count > 0) {
+            cout << " " << result[i].min_number << "\n";
+        }
+        else {
+            cout << "\n";
+        }
+    }
+
+    return 0;
 }
