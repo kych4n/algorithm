d08ddd93d5e37a188ab6306ead530661d70c3648
kych4n <164370868+kych4n@users.noreply.github.com>
2026-03-09 11:01:57 +0900

    [Gold V] Title: 기타 레슨, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2343. 기타 레슨/기타 레슨.cc
+++ 백준/Gold/2343. 기타 레슨/기타 레슨.cc
@@ -5,46 +5,58 @@ using namespace std;
 
 int N, M;
 
+/*
+    블루레이에는 총 N개의 강의가 들어가는데, 블루레이를 녹화할 때, 즉, i번 강의와 j번 강의를 같은 블루레이에 녹화하려면 i와 j 사이의 모든 강의도 같은 블루레이에 녹화해야 한다.
+
+    블루레이의 개수를 가급적 줄이려고 한다. 이때, 블루레이의 크기(녹화 가능한 길이)를 최소로 하려고 한다. 단, M개의 블루레이는 모두 같은 크기이어야 한다.
+
+    가능한 블루레이의 크기 중 최소를 구하는 프로그램을 작성하시오.
+
+    - N = 100000 -> O(NlogN)
+    - 블루레이 최소 크기를 찾아야 함 -> 매개변수 탐색
+    - left = 1, right = 전체 합
+    - target 정하고, 만든 블루레이의 개수가 M보다 작으면(target이 큼) -> target을 줄임
+                          블루레이의 개수가 M보다 크면(target이 작음) -> target을 늘림
+                          블루레이의 개수가 M과 같으면, 최솟값을 구해야 하기 때문에 -> target을 줄임
+*/
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
+    
     cin >> N >> M;
-    vector<int> lecture(N);
+    vector<int> lec(N);
+
     int left = 0, right = 0;
     for (int i = 0; i < N; i++) {
-        cin >> lecture[i];
-        left = max(left, lecture[i]);
-        right += lecture[i];
+        cin >> lec[i];
+        left = max(left, lec[i]);
+        right += lec[i];
     }
 
-    int result = right;
-
+    int result = INT32_MAX;
     while (left <= right) {
-        int mid = (left + right) / 2;
-        int bluray_count = 1;
-        int temp_sum = 0;
-
-        for (int i = 0; i < N; i++) {
-            if (temp_sum + lecture[i] > mid) {
-                bluray_count++;
-                temp_sum = lecture[i];
+        int target = (left + right) / 2;
+        int blulay_count = 1;
+        int cur_size = 0;
+        for (const int& l : lec) {
+            if (cur_size + l <= target) {
+                cur_size += l;
             }
             else {
-                temp_sum += lecture[i];
+                blulay_count++;
+                cur_size = l;
             }
         }
-
-        if (bluray_count <= M) {
-            result = min(result, mid);
-            right = mid - 1;
+        if (blulay_count <= M) {
+            result = min(result, target);
+            right = target - 1;
         }
-        else if (bluray_count > M) {
-            left = mid + 1;
+        else {
+            left = target + 1;
         }
     }
 
     cout << result << "\n";
-
     return 0;
 }
