16328bc2ecbeb8670a36c214f415f6a9f3ecaee7
kych4n <164370868+kych4n@users.noreply.github.com>
2026-03-11 11:14:23 +0900

    [Gold II] Title: 성냥개비, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/3687. 성냥개비/성냥개비.cc
+++ 백준/Gold/3687. 성냥개비/성냥개비.cc
@@ -3,47 +3,53 @@
 #include <algorithm>
 using namespace std;
 
+/*
+    성냥개비의 개수가 주어졌을 때, 성냥개비를 모두 사용해서 만들 수 있는 가장 작은 수와 큰 수를 찾는 프로그램을 작성하시오.
+*/
+
 typedef long long ll;
-ll num[8] = { 0,0,1,7,4,2,0,8 };
+ll need[8] = { 0,0,1,7,4,2,0,8 };
+vector<ll> dp(101, INT64_MAX);
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-   
-    vector<ll> dp(101, 1e18);
-    dp[2] = 1;
-    dp[3] = 7;
-    dp[4] = 4;
-    dp[5] = 2;
-    dp[6] = 6;
-    dp[7] = 8;
-    
+
+    dp[2] = 1, dp[3] = 7, dp[4] = 4, dp[5] = 2, dp[6] = 6, dp[7] = 8;
     for (int i = 8; i <= 100; i++) {
         for (int j = 2; j <= 7; j++) {
-            if (dp[i - j] == 1e18) continue;
-            dp[i] = min(dp[i], dp[i - j] * 10 + num[j]);
+            if (i - j < 2) continue;
+            dp[i] = min(dp[i], dp[i - j] * 10 + need[j]);
         }
     }
 
     int T;
     cin >> T;
-    for (int t = 0; t < T; t++) {
+    while (T--) {
         int N;
         cin >> N;
 
+        /*
+            가장 작은 수 
+        */
         cout << dp[N] << " ";
 
-        if (N % 2 == 0) {
-            for (int i = 0; i < N / 2; i++) {
-                cout << 1;
-            }
-        }
-        else {
+        /*
+            가장 큰 수 = 자릿수가 많아야 함 = 성냥개비를 적게 쓰는 것을 최대한 많이 사용
+            필요한 수가 적은 것이 1(2개 필요)과 7(3개 필요) -> 홀수, 짝수에 따라 구분해서 1과 7을 사용한다면 자릿수를 많이 늘릴 수 있음
+        */
+        if (N % 2 == 1) {
             cout << 7;
             for (int i = 0; i < (N - 3) / 2; i++) {
                 cout << 1;
             }
         }
+        else {
+            for (int i = 0; i < N / 2; i++) {
+                cout << 1;
+            }
+        }
+
         cout << "\n";
     }
 
