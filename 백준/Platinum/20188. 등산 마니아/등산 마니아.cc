7c70dfa32f902c5544bb654c89734781cef0f2d8
kych4n <164370868+kych4n@users.noreply.github.com>
2026-03-16 12:12:22 +0900

    [Platinum V] Title: 등산 마니아, Time: 640 ms, Memory: 60552 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/20188. 등산 마니아/등산 마니아.cc
+++ 백준/Platinum/20188. 등산 마니아/등산 마니아.cc
@@ -1,19 +1,23 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <map>
 #include <algorithm>
 using namespace std;
 
-typedef long long ll;
+/*
+    한 오두막에서 다른 오두막으로 갈 때는 1번을 거치는 가장 짧은 길을 따라 간다. 
+    모든 i번 오두막에서 j번 오두막으로 가는 길에 포함된 길의 개수의 총 합, 두 번 이상 지나간 길은 한 번만 센다는 것에 주의하라.
 
+    - N=10^5, 1에서부터 깊이를 늘려가면서 거리를 갱신하고, dp[i][j] = dp[1][i] + dp[k][j]은 에바임
+    - 길의 개수의 총합을 구해야 함 -> 길이 언제 사용되지 않는가? - i,j 간선이라면, j의 서브트리를 구성하는 노드
+*/
+typedef long long ll;
 int N;
 ll total_diversity = 0;
 ll subtree_size[300001];
 map<int, vector<int>> adj;
 
 ll comb2(ll n) {
-    if (n < 2) return 0;
     return n * (n - 1) / 2;
 }
 
@@ -22,10 +26,8 @@ void dfs(int cur, int prev) {
 
     for (int next : adj[cur]) {
         if (next == prev) continue;
-
         dfs(next, cur);
         subtree_size[cur] += subtree_size[next];
-
         total_diversity += comb2(N) - comb2(N - subtree_size[next]);
     }
 }
@@ -33,7 +35,7 @@ void dfs(int cur, int prev) {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    
+
     cin >> N;
     for (int i = 0; i < N - 1; i++) {
         int a, b;
@@ -43,8 +45,8 @@ int main() {
     }
 
     dfs(1, 0);
-
-    cout << total_diversity << "\n";
     
+    cout << total_diversity << "\n";
+
     return 0;
 }
