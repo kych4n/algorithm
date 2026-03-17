eb52e5e0a7fa58b08a270579d4ec39eb115ce41e
kych4n <164370868+kych4n@users.noreply.github.com>
2026-03-17 17:30:07 +0900

    [Gold IV] Title: 여행 가자, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.cc
+++ 백준/Gold/1976. 여행 가자/여행 가자.cc
@@ -1,65 +1,68 @@
 #include <iostream>
 #include <vector>
+#include <set>
 #include <algorithm>
 using namespace std;
 
+/*
+    도시들의 개수와 도시들 간의 연결 여부가 주어져 있고, 계획에 속한 도시들이 순서대로 주어졌을 때 가능한지 여부를 판별
+    - 최단 거리 아님, 그냥 가능 여부를 물어보고 있으므로, 한 덩어리로 뭉쳐있으면 됨
+    - 
+*/
 int N, M;
-vector<vector<int>> adj_list;
-vector<int> plan;
-vector<int> parent;
+int adj[200][200];
+int parent[200];
 
 int find_parent(int x) {
-	if (x != parent[x]) {
-		return find_parent(parent[x]);
-	}
-	return parent[x];
+    if (parent[x] != x) {
+        return find_parent(parent[x]);
+    }
+    return parent[x];
 }
 
 void union_parent(int x, int y) {
-	x = find_parent(x);
-	y = find_parent(y);
-	if (x < y) {
-		parent[y] = x;
-	}
-	else {
-		parent[x] = y;
-	}
+    x = find_parent(x);
+    y = find_parent(y);
+    if (x < y) {
+        parent[y] = x;
+    }
+    else {
+        parent[x] = y;
+    }
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	cin >> N >> M;
-	adj_list.resize(N, vector<int>(N));
-	parent.resize(N);
-	plan.resize(M);
-	for (int i = 0; i < N; i++) {
-		parent[i] = i;
-		for (int j = 0; j < N; j++) {
-			cin >> adj_list[i][j];
-		}
-	}
-	for (int i = 0; i < M; i++) {
-		cin >> plan[i];
-		plan[i]--;
-	}
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			if (adj_list[i][j] && parent[i] != parent[j]) {
-				union_parent(i, j);
-			}
-		}
-	}
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        parent[i] = i;
+        for (int j = 0; j < N; j++) {
+            cin >> adj[i][j];
+        }
+    }
 
-	for (int i = 0; i < M - 1; i++) {
-		int start = plan[i];
-		int end = plan[i + 1];
-		if (find_parent(start) != find_parent(end)) {
-			cout << "NO" << "\n";
-			return 0;
-		}
-	}
-	cout << "YES" << "\n";
-	return 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
+            if (adj[i][j]) union_parent(i, j);
+        }
+    }
+
+    int dest;
+    set<int> s;
+    for (int i = 0; i < M; i++) {
+        cin >> dest;
+        dest--;
+        s.insert(find_parent(dest));
+    }
+
+    if (s.size() == 1) {
+        cout << "YES\n";
+    }
+    else {
+        cout << "NO\n";
+    }
+
+    return 0;
 }
