ffbf64cffaf93f3a1e8d516319cbcf9da7cce071
kych4n <164370868+kych4n@users.noreply.github.com>
2026-03-17 19:28:55 +0900

    [Gold III] Title: 비슷한 단어, Time: 8 ms, Memory: 3644 KB -BaekjoonHub

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.cc
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.cc
@@ -1,75 +1,65 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <set>
 #include <string>
 #include <queue>
-#include <set>
 #include <algorithm>
 using namespace std;
 
+/*
+    접두사의 길이가 가장 긴 쌍을 출력, 여러 개일 때에는 입력되는 순서대로 제일 앞쪽에 있는 순으로 출력.
+    - 비슷함을 찾을 때는 문자열 정렬, 위치, 최대 접두사 길이
+*/
 int N;
-set<string> word;
-vector<pair<string, int>> word_loc;
-int max_prefix_count = 0;
-set<pair<string, int>> s;
 
-int get_prefix_count(string a, string b) {
-	int a_length = a.length();
-	int b_length = b.length();
-	int common_length = min(a_length, b_length);
-	int count = 0;
-	for (int i = 0; i < common_length; i++) {
-		if (a[i] == b[i]) {
-			count++;
-		}
-		else {
-			break;
-		}
-	}
-	return count;
+int get_prefix_length(const string& a, const string& b) {
+    int prefix_length = 0;
+    int min_length = min(a.length(), b.length());
+    for (int i = 0; i < min_length; i++) {
+        if (a[i] == b[i]) prefix_length++;
+        else break;
+    }
+    return prefix_length;
 }
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	cin >> N;
-	for (int i = 0; i < N; i++) {
-		string str;
-		cin >> str;
-		if (word.find(str) != word.end()) continue;
-		word.insert(str);
-		word_loc.push_back({ str, i });
-	}
-	
-	sort(word_loc.begin(), word_loc.end());
+    cin >> N;
+    vector<string> original(N);
+    vector<pair<string, int>> sorted(N);
+    for (int i = 0; i < N; i++) {
+        cin >> original[i];
+        sorted[i] = { original[i], i };
+    }
 
-	for (int i = 0; i < N - 1; i++) {
-		int prefix_count = get_prefix_count(word_loc[i].first, word_loc[i + 1].first);
-		if (max_prefix_count < prefix_count) {
-			max_prefix_count = prefix_count;
-			s.clear();
-			s.insert(word_loc[i]);
-			s.insert(word_loc[i + 1]);
-		}
-		else if (max_prefix_count == prefix_count) {
-			s.insert(word_loc[i]);
-			s.insert(word_loc[i + 1]);
-		}
-	}
+    sort(sorted.begin(), sorted.end());
 
-	vector<pair<int, string>> v;
-	for (auto p : s) {
-		v.push_back({ p.second, p.first });
-	}
-	sort(v.begin(), v.end());
-	cout << v[0].second << "\n";
+    int max_prefix_length = 0;
+    vector<int> mpl(N);
+    for (int i = 0; i < N - 1; i++) {
+        int current_prefix_length = get_prefix_length(sorted[i].first, sorted[i + 1].first);
+        max_prefix_length = max(max_prefix_length, current_prefix_length);
+        mpl[sorted[i].second] = max(mpl[sorted[i].second], current_prefix_length);
+        mpl[sorted[i + 1].second] = max(mpl[sorted[i + 1].second], current_prefix_length);
+    }
 
-	for (int i = 1; i < v.size(); i++) {
-		if (v[0].second.substr(0, max_prefix_count) == v[i].second.substr(0, max_prefix_count)) {
-			cout << v[i].second << "\n";
-			break;
-		}
-	}
+    int first_idx = -1;
+    for (int i = 0; i < N; i++) {
+        if (mpl[i] == max_prefix_length) {
+            cout << original[i] << "\n";
+            first_idx = i;
+            break;
+        }
+    }
+    for (int i = first_idx + 1; i < N; i++) {
+        if (get_prefix_length(original[first_idx], original[i]) == max_prefix_length) {
+            cout << original[i] << "\n";
+            break;
+        }
+    }
 
-	return 0;
+    return 0;
 }
