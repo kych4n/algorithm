c11f7ecfe020520391faa504de59d6966c169893
kych4n <164370868+kych4n@users.noreply.github.com>
2026-03-20 20:46:18 +0900

    [Gold IV] Title: 좋다, Time: 12 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.cc
+++ 백준/Gold/1253. 좋다/좋다.cc
@@ -1,25 +1,36 @@
 #include <iostream>
 #include <vector>
-#include <set>
 #include <algorithm>
 using namespace std;
 
+/*
+    어떤 수가 다른 수 두 개의 합으로 나타낼 수 있는 것이 몇 개인지 출력하라.
+    수의 위치가 다르면 값이 같아도 다른 수이다.
+
+    - 두 개의 합 -> 투 포인터 의심
+    - 정렬해두었음 -> i, j의 합, j+1부터 탐색
+        - i, j의 합보다 크다 -> i++
+        - i, j의 합과 같다 -> 체크하고 차이가 작은 것을 늘리기
+
+        - i, j의 합보다 모두 작다 -> 그럼 나머지 조합에서도 작으므로 끝내기
+*/
+
 int N;
 
-int main() {
+int main(void) {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
     cin >> N;
     vector<int> A(N);
-    vector<bool> visited(N);
     for (int i = 0; i < N; i++) {
         cin >> A[i];
     }
     sort(A.begin(), A.end());
-    
+
+    vector<bool> visited(N);
+
     for (int i = 0; i < N; i++) {
-        int target = A[i];
         int left = 0, right = N - 1;
         while (left < right) {
             if (left == i) {
@@ -30,24 +41,26 @@ int main() {
                 right--;
                 continue;
             }
-            int sum = A[left] + A[right];
-            if (target > sum) {
-                left++;
-            }
-            else if (target == sum) {
+            if (A[left] + A[right] == A[i]) {
                 visited[i] = true;
                 break;
             }
-            else {
+            else if (A[left] + A[right] > A[i]) {
                 right--;
+            }
+            else {
+                left++;
             }
         }
     }
 
     int result = 0;
     for (int i = 0; i < N; i++) {
-        if (visited[i]) result++;
+        if (visited[i]) {
+            result++;
+        }
     }
     cout << result << "\n";
+
     return 0;
 }
