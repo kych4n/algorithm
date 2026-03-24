95628ba194ede68f4cbffca58f8f4f3f3d7ca2ba
kych4n <164370868+kych4n@users.noreply.github.com>
2026-03-24 12:37:54 +0900

    [Gold II] Title: 동전 분배, Time: 504 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1943. 동전 분배/동전 분배.cc
+++ 백준/Gold/1943. 동전 분배/동전 분배.cc
@@ -1,57 +1,58 @@
 #include <iostream>
 #include <vector>
+#include <bitset>
 #include <algorithm>
 using namespace std;
 
 /*
-    N가지 종류의 동전을 각각 몇 개씩 주셨을 때, 그 돈을 반으로 나눌 수 있는지 없는지 판단
+    - N가지 종류의 동전을 각각 몇 개씩 주셨을 때, 
+    그 돈을 반으로 나눌 수 있는지 없는지 판단하는 것이다.
+    -> 전체 총합의 반을 구성할 수 있는지 판단
 
-    - 절반으로 나누기? 500 = 100 * 5이 가능한 경우도 있으므로 X
-    - 조합 만들기 -> 백트래킹X, 투포인터X, DP
-    - DP 최대 50000, 각 분배의 합을 의미
-    - 
+    
 */
 
-struct Coin {
+struct Money {
     int v, c;
 };
 
 int N;
 
-int main() {
+int main(void) {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    for (int t = 0; t < 3; t++) {
+    for (int tc = 0; tc < 3; tc++) {
+        int total = 0;
+
         cin >> N;
-        int total_val = 0;
-        vector<Coin> coins(N);
+        vector<Money> moneys(N);
         for (int i = 0; i < N; i++) {
-            cin >> coins[i].v >> coins[i].c;
-            total_val += coins[i].v * coins[i].c;
+            cin >> moneys[i].v >> moneys[i].c;
+            total += moneys[i].v * moneys[i].c;
         }
 
-        if (total_val % 2 != 0) {
+        if (total % 2 != 0) {
             cout << 0 << "\n";
             continue;
         }
 
-        int max_val = total_val / 2;
+        int max_val = total / 2;
         vector<bool> dp(max_val + 1);
         dp[0] = true;
 
-        for (const Coin& coin : coins) {
-            for (int cur_val = max_val; cur_val >= coin.v; cur_val--) {
-                if (dp[cur_val - coin.v]) {
-                    for (int k = 0; k < coin.c && cur_val + k * coin.v <= max_val; k++) {
-                        dp[cur_val + k * coin.v] = true;
+        for (const Money& m : moneys) {
+            if (dp[max_val]) break;
+            for (int target = max_val; target >= m.v; target--) {
+                if (dp[target - m.v]) {
+                    for (int k = 0; k < m.c && target + k * m.v <= max_val; k++) {
+                        dp[target + k * m.v] = true;
                     }
                 }
             }
         }
 
         cout << (dp[max_val] ? 1 : 0) << "\n";
-
     }
     
     return 0;
