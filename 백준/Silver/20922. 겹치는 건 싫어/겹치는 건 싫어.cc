9ef8b93e6437636632238e3efa7da22b73267df0
kych4n <164370868+kych4n@users.noreply.github.com>
2026-04-07 11:15:07 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 16 ms, Memory: 3196 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.cc
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.cc
@@ -4,30 +4,40 @@
 #include <algorithm>
 using namespace std;
 
-int n, k, a, result = 0;
-queue<int> q;
-vector<int> current_count(100001);
+/*
+    같은 원소가 K개 이하로 들어 있는 최장 연속 부분 수열의 길이
+
+    - N = 200000
+    - 원소 당 몇 개인지 기록
+    - right : 기록 증가 + 기록 증가했을 때 개수가 K개 초과면, left 증가 + 기록 감소
+*/
+
+int N, K;
 
 int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cin >> n >> k;
-	for (int i = 0; i < n; i++) {
-		cin >> a;
-		if (current_count[a] >= k) {
-			while (q.front() != a) {
-				current_count[q.front()] -= 1;
-				q.pop();
-			}
-			current_count[q.front()] -= 1;
-			q.pop();
-		}
-		if (current_count[a] < k) {
-			q.push(a);
-			current_count[a] += 1;
-			result = max(result, (int)q.size());
-		}
-	}
-	cout << result << "\n";
-	return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N >> K;
+    vector<int> a(N);
+    vector<int> count(100001);
+    for (int i = 0; i < N; i++) {
+        cin >> a[i];
+    }
+
+    int left, right; left = right = 0;
+    int result = 0;
+    while (right < N) {
+        count[a[right]]++;
+        while (count[a[right]] > K) {
+            count[a[left]]--;
+            left++;
+        }
+        result = max(result, right - left + 1);
+        right++;
+    }
+
+    cout << result << "\n";
+
+    return 0;
 }
