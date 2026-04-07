4a10de1248870e74847629f27c49cc53bc6fe41b
kych4n <164370868+kych4n@users.noreply.github.com>
2026-04-07 10:53:16 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 40 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.cc
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.cc
@@ -1,62 +1,72 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <cmath>
 #include <algorithm>
 using namespace std;
-int dx[] = { -1,1,0,0 };
-int dy[] = { 0,0 ,-1,1 };
 
-int n, m, target_x, target_y;
-vector<vector<int>> map(1000, vector<int>(1000));
-vector<vector<int>> save(1000, vector<int>(1000, 1e9 + 7));
+/*
 
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> map[i][j];
-			if (map[i][j] == 2) {
-				target_x = i;
-				target_y = j;
-				save[i][j] = 0;
-			}
-			else if (map[i][j] == 0) {
-				save[i][j] = 0;
-			}
-		}
-	}
-	queue<pair<int, int>> q;
-	q.push({ target_x, target_y });
-	while (!q.empty()) {
-		int cur_x = q.front().first;
-		int cur_y = q.front().second;
-		q.pop();
-		for (int i = 0; i < 4; i++) {
-			int new_x = cur_x + dx[i];
-			int new_y = cur_y + dy[i];
-			if (new_x >= 0 && new_x < n && new_y >= 0 && new_y < m) {
-				if (save[new_x][new_y] > save[cur_x][cur_y] + 1) {
-					save[new_x][new_y] = save[cur_x][cur_y] + 1;
-					q.push({ new_x, new_y });
-				}
-			}
-		}
-	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (save[i][j] == 1e9 + 7) {
-				cout << -1 << " ";
-			}
-			else {
-				cout << save[i][j] << " ";
-			}
-		}
-		cout << "\n";
-	}
+*/
 
-	return 0;
+struct Point {
+    int r, c;
+    Point() {
+
+    }
+    Point(int r, int c) {
+        this->r = r;
+        this->c = c;
+    }
+};
+
+int N, M;
+int land[1000][1000];
+int cost[1000][1000];
+Point start;
+int dr[] = { -1,1,0,0 };
+int dc[] = { 0,0,-1,1 };
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> land[i][j];
+            if (land[i][j] == 2) {
+                start = Point(i, j);
+            }
+            cost[i][j] = -1;
+        }
+    }
+    
+    queue<Point> q;
+    q.push(start);
+    cost[start.r][start.c] = 0;
+    while (!q.empty()) {
+        Point cur = q.front();
+        q.pop();
+        for (int i = 0; i < 4; i++) {
+            Point next = Point(cur.r + dr[i], cur.c + dc[i]);
+            if (next.r >= 0 && next.r < N && next.c >= 0 && next.c < M && land[next.r][next.c] != 0 && cost[next.r][next.c] == -1) {
+                cost[next.r][next.c] = cost[cur.r][cur.c] + 1;
+                q.push(next);
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (land[i][j] == 0) {
+                cout << 0 << " ";
+            }
+            else {
+                cout << cost[i][j] << " ";
+            }
+        }
+        cout << "\n";
+    }
+
+    return 0;
 }
