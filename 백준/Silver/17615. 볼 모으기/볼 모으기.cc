b92a75a324a9fcb597cf051692d6a3388e4b5203
kych4n <164370868+kych4n@users.noreply.github.com>
2026-04-09 10:01:43 +0900

    [Silver I] Title: 볼 모으기, Time: 12 ms, Memory: 2912 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.cc
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.cc
@@ -1,64 +1,58 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-int n;
+/*
+    빨간공 왼/오
+    파란공 왼/오
+
+    내가 target을 오른쪽에 두려고 함
+    오른쪽에서 왼쪽으로 가다가 target이 아닌 것이 나오면 이후에 target이 나왔을 때 건너뛰는 것이 필요함
+*/
+
+int N, result = 1e9 + 7;
 string str;
-int result = 1e9 + 7;
-vector<pair<char, int>> info;
 
-void start_front(char target) {
-	bool flag = false;
-	int temp_result = 0;
-	for (pair<char, int> x : info) {
-		if (x.first != target) {
-			flag = true;
-		}
-		if (flag && x.first == target) {
-			temp_result += x.second;
-		}
-	}
-	result = min(result, temp_result);
+void move_right(char target) {
+    bool need_move = false;
+    int temp_result = 0;
+    for (int i = N - 1; i >= 0; i--) {
+        if (target != str[i]) {
+            need_move = true;
+        }
+        else if (need_move) {
+            temp_result += 1;
+        }
+    }
+    result = min(result, temp_result);
 }
 
-void start_back(char target) {
-	bool flag = false;
-	int temp_result = 0;
-	for (int i = info.size() - 1; i >= 0; i--) {
-		if (info[i].first != target) {
-			flag = true;
-		}
-		if (flag && info[i].first == target) {
-			temp_result += info[i].second;
-		}
-	}
-	result = min(result, temp_result);
+void move_left(char target) {
+    bool need_move = false;
+    int temp_result = 0;
+    for (int i = 0; i < N; i++) {
+        if (target != str[i]) {
+            need_move = true;
+        }
+        else if (need_move) {
+            temp_result += 1;
+        }
+    }
+    result = min(result, temp_result);
 }
-
 
 int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cin >> n >> str;
-	char current_char = str[0];
-	int current_count = 1;
-	for (int i = 1; i < n; i++) {
-		if (current_char == str[i]) {
-			current_count += 1;
-		}
-		else {
-			info.push_back({ current_char, current_count });
-			current_char = str[i];
-			current_count = 1;
-		}
-	}
-	info.push_back({ current_char, current_count });
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	start_front('R');
-	start_front('B');
-	start_back('R');
-	start_back('B');
+    cin >> N >> str;
+    move_left('B');
+    move_left('R');
+    move_right('B');
+    move_right('R');
 
-	cout << result << "\n";
-	return 0;
+    cout << result << "\n";
+
+    return 0;
 }
