fe2b0880794fc98a2f1ecfa6c1f9f4d01a662b2c
kych4n <164370868+kych4n@users.noreply.github.com>
2026-04-14 09:54:37 +0900

    [Silver I] Title: 문자열 교환, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1522. 문자열 교환/문자열 교환.cc
+++ 백준/Silver/1522. 문자열 교환/문자열 교환.cc
@@ -1,35 +1,46 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
+/*
+    [입력] a와 b로만 이루어진 문자열
+    [출력] a를 모두 연속으로 만들기 위해서 필요한 교환의 최소 횟수
+
+    N = 1000
+    O(N*N)
+
+    - a를 연속으로 만드는 것 = b를 연속으로 만드는 것
+
+*/
+
 string str;
-int b_count = 0, result = 1e9 + 7;
 
 int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cin >> str;
-	int length = str.length();
-	for (int i = 0; i < length; i++) {
-		if (str[i] == 'b') {
-			b_count += 1;
-		}
-	}
-	if (b_count == 0) {
-		cout << 0 << "\n";
-		return 0;
-	}
-	for (int i = 0; i < length; i++) {
-		int current_b_count = 0;
-		for (int j = 0; j < length; j++) {
-			if (str[(i + j) % length] == 'b') {
-				current_b_count += 1;
-			}
-			if (j + 1 >= b_count) {
-				result = min(result, j + 1 - current_b_count);
-			}
-		}
-	}
-	cout << result << "\n";
-	return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> str;
+    
+    int N = str.length();
+    int a_count = 0;
+    for (int i = 0; i < N; i++) {
+        if (str[i] == 'a') {
+            a_count++;
+        }
+    }
+
+    int result = 1e9 + 7;
+    
+    for (int i = 0; i < N; i++) {
+        int temp_result = 0;
+        for (int j = 0; j < a_count; j++) {
+            if (str[(i + j) % N] == 'b') temp_result++;
+        }
+        result = min(result, temp_result);
+    }
+
+    cout << result << "\n";
+
+    return 0;
 }
