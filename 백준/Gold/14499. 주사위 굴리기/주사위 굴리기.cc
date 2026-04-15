79c4563636df9ff2842e21ae8f07c5f0da4e30dc
kych4n <164370868+kych4n@users.noreply.github.com>
2026-04-15 09:32:13 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.cc
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.cc
@@ -1,76 +1,94 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <cmath>
+#include <string>
+#include <cctype>
 #include <algorithm>
 using namespace std;
-vector<vector<int>> map(20, vector<int>(20));
-vector<int> dice(7);
-int n, m, x, y, k, command, temp;
 
+/*
+    [입력] 명령
+    [출력] 이동할 때마다 주사위 윗 면에 있는 수
+    
+    - 주사위의 각 칸이 어떤 수를 가지고 있는지 저장해야 함
+    - 이동 시 주사위의 각 칸이 어떻게 변하는가 -> 이동한 상태에서 6번이 0이 아니면 바닥으로 복사 + 6번은 0
+*/
 
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	cin >> n >> m >> x >> y >> k;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> map[i][j];
-		}
-	}
-	for (int u = 0; u < k; u++) {
-		cin >> command;
-		temp = dice[6];
-		if (command == 1) {
-			if (y + 1 >= m) {
-				continue;
-			}
-			y++;
-			dice[6] = dice[3];
-			dice[3] = dice[1];
-			dice[1] = dice[4];
-			dice[4] = temp;
-		}
-		else if (command == 2) {
-			if (y - 1 < 0) {
-				continue;
-			}
-			y--;
-			dice[6] = dice[4];
-			dice[4] = dice[1];
-			dice[1] = dice[3];
-			dice[3] = temp;
-		}
-		else if (command == 3) {
-			if (x - 1 < 0) {
-				continue;
-			}
-			x--;
-			dice[6] = dice[2];
-			dice[2] = dice[1];
-			dice[1] = dice[5];
-			dice[5] = temp;
-		}
-		else if (command == 4) {
-			if (x + 1 >= n) {
-				continue;
-			}
-			x++;
-			dice[6] = dice[5];
-			dice[5] = dice[1];
-			dice[1] = dice[2];
-			dice[2] = temp;
-		}
-		if (map[x][y] == 0) {
-			map[x][y] = dice[6];
-		}
-		else {
-			dice[6] = map[x][y];
-			map[x][y] = 0;
-		}
-		cout << dice[1] << "\n";
-	}
+int N, M, x, y, K;
+int board[20][20];
+int dr[] = { -1,0,0,-1,1 };
+int dc[] = { -1,1,-1,0,0 };
+int dice[7];
+int cmd;
 
-	return 0;
+void roll_east() {
+    int temp = dice[1];
+    dice[1] = dice[4];
+    dice[4] = dice[6];
+    dice[6] = dice[3];
+    dice[3] = temp;
+}
+
+void roll_west() {
+    int temp = dice[1];
+    dice[1] = dice[3];
+    dice[3] = dice[6];
+    dice[6] = dice[4];
+    dice[4] = temp;
+}
+
+void roll_north() {
+    int temp = dice[1];
+    dice[1] = dice[5];
+    dice[5] = dice[6];
+    dice[6] = dice[2];
+    dice[2] = temp;
+}
+
+void roll_south() {
+    int temp = dice[1];
+    dice[1] = dice[2];
+    dice[2] = dice[6];
+    dice[6] = dice[5];
+    dice[5] = temp;
+}
+
+void roll(int cmd) {
+    if (cmd == 1) roll_east();
+    else if (cmd == 2) roll_west();
+    else if (cmd == 3) roll_north();
+    else if (cmd == 4) roll_south();
+}
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    for (int i = 1; i <= 6; i++) dice[i] = 0;
+
+    cin >> N >> M >> x >> y >> K;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> board[i][j];
+        }
+    }
+
+    for (int i = 0; i < K; i++) {
+        cin >> cmd;
+        if (!(x + dr[cmd] >= 0 && x + dr[cmd] < N && y + dc[cmd] >= 0 && y + dc[cmd] < M)) {
+            continue;
+        }
+        x += dr[cmd];
+        y += dc[cmd];
+        roll(cmd);
+        if (board[x][y] == 0) {
+            board[x][y] = dice[6];
+        }
+        else {
+            dice[6] = board[x][y];
+            board[x][y] = 0;
+        }
+        cout << dice[1] << "\n";
+    }
+
+    return 0;
 }
